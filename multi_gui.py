ca52b0004377763e2d07cafe68997cff0526bfa2
Daniel Kratzert <dkratzert@gmx.de>
2019-06-12 18:12:57 +0200

    try drag and drop order

--- multi_gui.py
+++ multi_gui.py
@@ -2,12 +2,12 @@ import os
 import sys
 from pathlib import Path
 
-DEBUG = False
+DEBUG = True
 
 if DEBUG:
     from PyQt5 import uic
 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QTreeWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QListWidgetItem
 
 # This is to make sure that multitable finds the application path even when it is
 # executed from another path e.g. when opened via "open file" in windows:
@@ -31,20 +31,19 @@ class AppWindow(QMainWindow):
         self.ui.report_button.setDisabled(True)
         self.ui.removeButton.setDisabled(True)
         self.connect_signals_and_slots()
-        self.ui.CifFileListTreeWidget.resizeColumnToContents(0)
 
     def connect_signals_and_slots(self):
         self.ui.cif_files_button.clicked.connect(self.add_files_to_list)
         self.ui.removeButton.clicked.connect(self.remove_file)
         self.ui.report_button.clicked.connect(self.make_report)
-        self.ui.CifFileListTreeWidget.itemClicked.connect(self.toggle_remove)
+        self.ui.CifFileListListWidget.itemClicked.connect(self.toggle_remove)
 
     def toggle_remove(self, selection):
         selected = False
-        for num in range(0, self.ui.CifFileListTreeWidget.topLevelItemCount()):
-            if self.ui.CifFileListTreeWidget.topLevelItem(num).isSelected():
+        for num in range(0, self.ui.CifFileListListWidget.count()):
+            if self.ui.CifFileListListWidget.item(num).isSelected():
                 selected = True
-        if selected == True:
+        if selected:
             self.ui.removeButton.setEnabled(True)
         else:
             self.ui.removeButton.setDisabled(True)
@@ -53,7 +52,7 @@ class AppWindow(QMainWindow):
         """
         Add files to the files list.
         """
-        self.ui.CifFileListTreeWidget.clear()
+        self.ui.CifFileListListWidget.clear()
         if not files:
             files = self.get_files_from_dialog()
         if files:
@@ -63,25 +62,17 @@ class AppWindow(QMainWindow):
             return
         for n, file in enumerate(files):
             if file:
-                cif_tree_item = QTreeWidgetItem()
-                self.ui.CifFileListTreeWidget.addTopLevelItem(cif_tree_item)
-                cif_tree_item.setText(0, file)
-                # cif_tree_item.currentItemChanged.connect(self.toggle_remove("selected"))
-                # button = QPushButton("remove")
-                # self.ui.CifFileListTreeWidget.setItemWidget(cif_tree_item, 1, button)
-                # button.setMinimumWidth(80)
-                # button.setMaximumWidth(80)
-                # button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Minimum)
-                # button.clicked.connect(self.remove_file)
-        self.ui.CifFileListTreeWidget.resizeColumnToContents(0)
+                cif_tree_item = QListWidgetItem()
+                self.ui.CifFileListListWidget.addItem(cif_tree_item)
+                cif_tree_item.setText(file)
 
     def remove_file(self):
         """
         Removes the currently selected file from list.
         """
-        sel = self.ui.CifFileListTreeWidget.selectionModel().selection()
-        if sel.indexes()[0]:
-            self.ui.CifFileListTreeWidget.takeTopLevelItem(sel.indexes()[0].row())
+        sel = self.ui.CifFileListListWidget.selectionModel().selection()
+        for s in sel.indexes():
+            self.ui.CifFileListListWidget.takeItem(s.row())
 
     def get_files_from_dialog(self):
         """
@@ -96,9 +87,9 @@ class AppWindow(QMainWindow):
     def make_report(self):
         files_list = []
         self.ui.OutputTextEdit.clear()
-        for num in range(self.ui.CifFileListTreeWidget.topLevelItemCount()):
-            item = self.ui.CifFileListTreeWidget.topLevelItem(num)
-            itemtxt = item.text(0)
+        for num in range(self.ui.CifFileListListWidget.count()):
+            item = self.ui.CifFileListListWidget.item(num)
+            itemtxt = item.text()
             files_list.append(itemtxt)
             self.ui.OutputTextEdit.append(Path(itemtxt).name)
         if not files_list:
@@ -109,7 +100,7 @@ class AppWindow(QMainWindow):
                                                          initialFilter='*.docx')
         multitable.make_report_from(files_list, output_filename)
         self.ui.OutputTextEdit.append('\nReport finished - output file: {}'.format(output_filename))
-        self.ui.CifFileListTreeWidget.clear()
+        self.ui.CifFileListListWidget.clear()
 
 
 if __name__ == '__main__':
@@ -119,7 +110,7 @@ if __name__ == '__main__':
 
     # Trick to make pyinstaller work: 
     p = Path('templates/default.docx')
-    
+
     app = QApplication(sys.argv)
     w = AppWindow()
     w.show()
