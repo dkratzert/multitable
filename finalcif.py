227201c6df24c36d980d3911050065e56b9d6d7e
Daniel Kratzert <dkratzert@gmx.de>
2019-06-13 11:05:49 +0200

    more TODO

--- finalcif.py
+++ finalcif.py
@@ -26,12 +26,18 @@ TODO:
 
 - the working directory has to be the directory of the currently opened cif file
 - open cif file and parse it. (With gemmi?)
-- put all incomplete information in the CifItemsTable. (add checkbox to be able to edit all cif values?)
+- put all incomplete information in the CifItemsTable. 
+  - Add checkbox to be able to edit all cif values?
+  - Or show all ? items first, then all others.
+- make first and second column of CifItemsTable uneditable.
+- Own data in CifItemsTable overrides From Data Source. 
+  (maybe with a signal to grey out the data source onEdit of Own Data)
 - make "save cif" work.
 - think about a possibility to save and edit templates: QSettings()
 - signal: if edit template clicked -> got to TemplatesStackedWidgetPage1
 - signal: if Save template clicked -> got to TemplatesStackedWidgetPage0
 - signal: if delete clicked -> delete current template table line
+- signal: if edit Own Data field -> grey out From Data Source field in same line.
 - action: rightclick on a template -> offer "delete template"
 - action: rightclick on a template -> offer "export template (to .cif)"
 - action: rightclick on a template -> offer "import template (from .cif)"
@@ -43,6 +49,8 @@ TODO:
 - SaveResidualsTableButton -> run multitable.py
 - SaveFullReportButton -> generate full report with description text and all tables as .docx (and pdf?)
   maybe also a preview? Directly open in MSword/LibreOffice?
+
+- save cif file with "name_fin.cif"
 
 - Add button for checkcif report.
 
@@ -59,6 +67,7 @@ class AppWindow(QMainWindow):
         hheader = self.ui.CifItemsTable.horizontalHeader()
         hheader.setSectionResizeMode(0, QHeaderView.Stretch)
         hheader.setSectionResizeMode(1, QHeaderView.Stretch)
+        hheader.setSectionResizeMode(2, QHeaderView.Stretch)
         hheader.setAlternatingRowColors(True)
 
     def add_new_datafile(self, n: int, label_text: str, placeholder: str = ''):
